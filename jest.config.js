c3d43d5506772ec7c9329714ebcfaa5d0cdc1e3c
hozt <jeff@hozt.com>
2024-08-29 21:54:25 -0700

    Add api directory

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-    moduleFileExtensions: ['js', 'jsx'],
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  };
