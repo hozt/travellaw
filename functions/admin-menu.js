be916cbbdd3fe4aeaa4908dcec0522cee512e375
hozt <jeff@hozt.com>
2024-08-30 14:53:39 -0700

    show menu when validated

--- functions/admin-menu.js
+++ functions/admin-menu.js
@@ -19,52 +19,48 @@ export async function onRequest(context) {
       const regenerateUrl = `${editorUrl}admin.php?page=custom_webhook`;
 
       const menuHTML = `
-        <div class="z-10 bg-gray-500 admin-menu">
-          <ul class="sticky top-0 left-0 right-0 flex pl-2 text-sm list-none">
+        <li>
+            <a href="${editorUrl}" target="_blank" rel="noreferrer">
+            <i class="icon-[mdi--view-dashboard] text-lg mr-1" title="Dashboard"></i>
+            <span class="hidden md:inline-block">Dashboard</span>
+            </a>
+        </li>
+        ${editPostUrl ? `
             <li>
-              <a href="${editorUrl}" target="_blank" rel="noreferrer">
-                <i class="icon-[mdi--view-dashboard] text-lg mr-1" title="Dashboard"></i>
-                <span class="hidden md:inline-block">Dashboard</span>
-              </a>
+            <a href="${editPostUrl}" target="_blank" rel="noreferrer">
+                <i class="icon-[mdi--post-it-note-edit] text-lg mr-1" title="Edit Post"></i>
+                <span class="hidden md:inline-block">Edit Post</span>
+            </a>
             </li>
-            ${editPostUrl ? `
-              <li>
-                <a href="${editPostUrl}" target="_blank" rel="noreferrer">
-                  <i class="icon-[mdi--post-it-note-edit] text-lg mr-1" title="Edit Post"></i>
-                  <span class="hidden md:inline-block">Edit Post</span>
-                </a>
-              </li>
-            ` : ''}
-            ${editPageUrl ? `
-              <li>
-                <a href="${editPageUrl}" target="_blank" rel="noreferrer">
-                  <i class="icon-[mdi--post-it-note-edit] text-lg mr-1" title="Edit Page"></i>
-                  <span class="hidden md:inline-block">Edit Page</span>
-                </a>
-              </li>
-            ` : ''}
-            ${editTemplateUrl ? `
-              <li>
-                <a href="${editTemplateUrl}" target="_blank" rel="noreferrer">
-                  <i class="icon-[mdi--post-it-note-edit] text-lg mr-1" title="Edit Template"></i>
-                  <span class="hidden md:inline-block">Edit Template</span>
-                </a>
-              </li>
-            ` : ''}
+        ` : ''}
+        ${editPageUrl ? `
             <li>
-              <a href="${editMenuUrl}" target="_blank" rel="noreferrer">
-                <i class="icon-[mdi--menu-open] text-lg mr-1" title="Edit Menu"></i>
-                <span class="hidden md:inline-block">Edit Menus</span>
-              </a>
+            <a href="${editPageUrl}" target="_blank" rel="noreferrer">
+                <i class="icon-[mdi--post-it-note-edit] text-lg mr-1" title="Edit Page"></i>
+                <span class="hidden md:inline-block">Edit Page</span>
+            </a>
             </li>
+        ` : ''}
+        ${editTemplateUrl ? `
             <li>
-              <a href="${regenerateUrl}" target="_blank" rel="noreferrer">
-                <i class="icon-[mdi--publish] text-lg mr-1" title="Regenerate Site"></i>
-                <span class="hidden md:inline-block">Regenerate Site</span>
-              </a>
+            <a href="${editTemplateUrl}" target="_blank" rel="noreferrer">
+                <i class="icon-[mdi--post-it-note-edit] text-lg mr-1" title="Edit Template"></i>
+                <span class="hidden md:inline-block">Edit Template</span>
+            </a>
             </li>
-          </ul>
-        </div>
+        ` : ''}
+        <li>
+            <a href="${editMenuUrl}" target="_blank" rel="noreferrer">
+            <i class="icon-[mdi--menu-open] text-lg mr-1" title="Edit Menu"></i>
+            <span class="hidden md:inline-block">Edit Menus</span>
+            </a>
+        </li>
+        <li>
+            <a href="${regenerateUrl}" target="_blank" rel="noreferrer">
+            <i class="icon-[mdi--publish] text-lg mr-1" title="Regenerate Site"></i>
+            <span class="hidden md:inline-block">Regenerate Site</span>
+            </a>
+        </li>
       `;
 
       return new Response(menuHTML, {
