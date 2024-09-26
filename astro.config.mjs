4f17881f465799980ce34b2ad57aea7e2385f8b2
hozt <jeff@hozt.com>
2024-09-25 17:08:51 -0700

    Fix analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite';
 import tailwind from "@astrojs/tailwind";
 import pagefind from "astro-pagefind";
 import compressor from "astro-compressor";
+import partytown from '@astrojs/partytown';
 
 const site = process.env.SITE_URL;
 
 export default defineConfig({
-  output: 'static',
-  build: {
-    format: "file"
-  },
   vite: {
     resolve: {
       preserveSymlinks: true
@@ -20,7 +17,12 @@ export default defineConfig({
       fileExtensions: [".html"]
     }),
     tailwind(),
-    pagefind()
+    pagefind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })
   ],
   site: site
 });
